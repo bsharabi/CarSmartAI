715b5b86754dc4410ad2410965026b8056c663f6
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2024-07-06 20:56:25 +0300

    fix motor_A func

--- RobotMove.py
+++ RobotMove.py
@@ -3,7 +3,8 @@ import RPi.GPIO as GPIO # type: ignore
 import settings
 
 class RobotMove:
-    def __init__(self, pwm_freq=1000):
+    
+    def __init__(self):
         """
         Initialize the RobotMove class with motor pins and PWM frequency.
 
@@ -19,7 +20,7 @@ class RobotMove:
         self.Dir_forward = 0
         self.Dir_backward = 1
 
-        self.pwm_freq = pwm_freq
+        self.pwm_freq = settings.PWM_FREQUENCY_ENGINE
         self.pwm_A = None
         self.pwm_B = None
 
@@ -133,12 +134,13 @@ def main():
 
     try:
         print("Moving forward")
-        robot.move(100, 'forward')
-        time.sleep(1.3)
-        robot.motor_stop()
+        robot.motor_A(50,'forward')
+        # robot.move(100, 'forward')
+        # time.sleep(1.3)
+        # robot.motor_stop()
         
-        print("Moving Backward")
-        robot.move(100, 'backward')
+        # print("Moving Backward")
+        # robot.move(100, 'backward')
         time.sleep(1.3)
         robot.motor_stop()
        
