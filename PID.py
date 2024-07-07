5111e99b388c6d9247d68942d275171e0277f9b1
liavm1998 <liavm12345@gmail.com>
2024-07-07 13:03:04 +0300

    obstacle ahead manevour and fix backward solution

---
+++ PID.py
@@ -0,0 +1,15 @@
+class PID:
+    def __init__(self, Kp, Ki, Kd, setpoint=0):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.setpoint = setpoint
+        self.last_error = 0
+        self.integral = 0
+
+    def update(self, current_value):
+        error = self.setpoint - current_value
+        self.integral += error
+        derivative = error - self.last_error
+        self.last_error = error
+        return self.Kp * error + self.Ki * self.integral + self.Kd * derivative
