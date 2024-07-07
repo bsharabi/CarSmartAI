2c6d66bd5dddc8dd8fa45bc52d21be456228e3d5
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2024-07-07 11:52:01 +0300

    test

--- testSpeech.py
+++ testSpeech.py
@@ -5,12 +5,12 @@ v_command=''
 speed_set = 80
 
 
-p = pyaudio.PyAudio()
-info = p.get_host_api_info_by_index(0)
-numdevices = info.get('deviceCount')
-for i in range(0, numdevices):
-        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-            print ("Input Device id "+str(i)+" - "+p.get_device_info_by_host_api_device_index(0, i).get('name'))
+# p = pyaudio.PyAudio()
+# info = p.get_host_api_info_by_index(0)
+# numdevices = info.get('deviceCount')
+# for i in range(0, numdevices):
+#         if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+#             print ("Input Device id "+str(i)+" - "+p.get_device_info_by_host_api_device_index(0, i).get('name'))
 
 # def run():
 #     global v_command
@@ -39,27 +39,27 @@ for i in range(0, numdevices):
 # # run()
 
 
-# import speech_recognition as sr
+import speech_recognition as sr
 
-# r = sr.Recognizer()
-# m = sr.Microphone(2)
+r = sr.Recognizer()
+m = sr.Microphone(0)
 
-# try:
-#     print("A moment of silence, please...")
-#     with m as source: r.adjust_for_ambient_noise(source)
-#     print("Set minimum energy threshold to {}".format(r.energy_threshold))
-#     while True:
-#         print("Say something!")
-#         with m as source: audio = r.listen(source)
-#         print("Got it! Now to recognize it...")
-#         try:
-#             # recognize speech using Google Speech Recognition
-#             value = r.recognize_google(audio)
+try:
+    print("A moment of silence, please...")
+    with m as source: r.adjust_for_ambient_noise(source)
+    print("Set minimum energy threshold to {}".format(r.energy_threshold))
+    while True:
+        print("Say something!")
+        with m as source: audio = r.listen(source)
+        print("Got it! Now to recognize it...")
+        try:
+            # recognize speech using Google Speech Recognition
+            value = r.recognize_google(audio)
 
-#             print("You said {}".format(value))
-#         except sr.UnknownValueError:
-#             print("Oops! Didn't catch that")
-#         except sr.RequestError as e:
-#             print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
-# except KeyboardInterrupt:
-#     pass
+            print("You said {}".format(value))
+        except sr.UnknownValueError:
+            print("Oops! Didn't catch that")
+        except sr.RequestError as e:
+            print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+except KeyboardInterrupt:
+    pass
